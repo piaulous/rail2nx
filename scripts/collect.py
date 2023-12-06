254c77d2ac62fde0155350e86a10de929d63b0fa
Piaulous <paul.dubielzig@mailbox.org>
2023-12-06 21:50:45 +0100

    update lines to graph fct

--- scripts/collect.py
+++ scripts/collect.py
@@ -6,6 +6,7 @@ import zipfile
 import geopandas as gpd
 import pandas as pd
 import requests
+from shapely.geometry import Point
 
 from scripts.tools import read_config
 
@@ -134,6 +135,30 @@ def get_rail_stations(scope):
         .to_crs(cfg["proj"]["crs_eur"])
     )
 
+    bus_operators = ["busbud_id", "distribusion_id", "flixbus_id"]
+    rail_operators = [
+        "sncf_id",
+        "entur_id",
+        "db_id",
+        "cff_id",
+        "leoexpress_id",
+        "obb_id",
+        "ouigo_id",
+        "trenitalia_id",
+        "trenord_id",
+        "ntv_id",
+        "hkx_id",
+        "renfe_id",
+        "atoc_id",
+        "benerail_id",
+        "westbahn_id",
+    ]
+
+    stations["has_bus_id"] = stations[bus_operators].notnull().any(axis=1)
+    stations["has_rail_id"] = stations[rail_operators].notnull().any(axis=1)
+
+    stations = stations.loc[stations["has_rail_id"]]
+
     if isinstance(scope, str):
         if len(scope) == 2:
             stations = stations[stations.country == scope]
@@ -145,3 +170,65 @@ def get_rail_stations(scope):
         TypeError("Please refer to the format for parameter 'scope'.")
 
     return stations
+
+
+def locate_missing_coords(stations_gdf):
+    fname = "data/stations_raw.csv"
+    nom = cfg["nominatim"]
+    stations = stations_gdf[stations_gdf.geometry.is_empty].set_crs(
+        cfg["proj"]["crs_def"], allow_override=True
+    )
+
+    unlocatables = []
+
+    for idx, row in stations.iterrows():
+        query = (
+            f"{nom['url']}&"
+            f"accept-language={nom['language']}&"
+            f"format={nom['format']}&"
+            f"q={row.slug}+{nom['tags']}&"
+            f"countrycodes={row.country}&"
+            f"addressdetails=1"
+        )
+        response = requests.get(query)
+
+        if response.json():
+            is_city = "f"  # found station
+            osm = response.json()[0]
+
+        else:
+            query = query.replace(
+                f"+{nom['tags']}", ""
+            )  # remove railway / station tag from query
+            response = requests.get(query)
+            matched_entries = [
+                entry for entry in response.json() if entry["class"] == "place"
+            ]
+
+            if matched_entries:
+                is_city = "t"  # place with station's name has been found
+                osm = matched_entries[0]
+
+            else:
+                logger.info(
+                    f"Could not locate station '{row['name']}', "
+                    f"entry will be removed."
+                )
+                unlocatables.append(idx)
+                continue
+
+        # fill missing data
+        stations.at[idx, "latitude"] = float(osm["lat"])
+        stations.at[idx, "longitude"] = float(osm["lon"])
+        stations.at[idx, "geometry"] = Point(osm["lon"], osm["lat"])
+        stations.at[idx, "is_city"] = is_city
+
+    stations = stations.to_crs(cfg["proj"]["crs_eur"])
+    stations_gdf.update(stations)
+
+    # update coords and drop stations that could not be located
+    if len(stations):
+        stations_csv = pd.read_csv(fname, low_memory=False)
+        stations_csv.update(stations)
+        stations_csv.drop(unlocatables, axis=0, inplace=True)
+        stations_csv.to_csv(fname)

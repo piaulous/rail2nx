a8009113d8fd2fc21f2b6f9cc97a09bd069738ab
Piaulous <paul.dubielzig@mailbox.org>
2023-12-15 13:04:10 +0100

    new function join stations to graph

--- scripts/network.py
+++ scripts/network.py
@@ -1,7 +1,14 @@
 import geopandas as gpd
 import networkx as nx
+import pandas as pd
 from shapely import set_precision
-from shapely.geometry import Point
+from shapely.geometry import LineString, Point
+from shapely.ops import nearest_points, snap, split
+
+from scripts.tools import read_config
+
+cfg = read_config()
+pd.options.mode.chained_assignment = None
 
 
 def lines_to_graph(lines_gdf):
@@ -60,7 +67,7 @@ def graph_to_gdfs(graph):
             graph (nx.MultiDiGraph): graph representation of lines_gdf
 
     Returns:
-            gpd.GeoDataFrames or tuple: gdf_nodes or gdf_edges or tuple
+            gpd.GeoDataFrames or tuple: gdf_nodes and gdf_edges as tuple
             of (gdf_nodes, gdf_edges). gdf_nodes is indexed by (x, y)
             coordinate and gdf_edges is multi-indexed by (u, v, key)
             following normal nx.MultiDiGraph structure.
@@ -86,5 +93,99 @@ def graph_to_gdfs(graph):
     gdf_edges["v"] = v
     gdf_edges["k"] = k
     gdf_edges.set_index(["u", "v", "k"], inplace=True)
+
+    return gdf_nodes, gdf_edges
+
+
+def join_stations_to_graph(graph, gdf_stations):
+    """
+    Station coordinates are shifted to nearest point on rail lines. Lines
+    are segmented based on nearest point coordinates. In case of duplicate
+    nearest point allocation synthetic edges are added.
+
+    Parameters:
+            graph (nx.MultiDiGraph): graph representation of lines_gdf
+
+    Returns:
+            gpd.GeoDataFrames or tuple: gdf_nodes and gdf_edges as tuple
+            of (gdf_nodes, gdf_edges). Columns in graph gdfs contain all
+            relevant attributes from lines_gdf / stations_gdf.
+    """
+
+    # prepare graph gdfs
+    gdf_nodes, gdf_edges = graph_to_gdfs(graph)
+    gdf_nodes["station"] = False
+    gdf_edges["segmented"] = False
+    gdf_edges["line_geom"] = gdf_edges.geometry
+
+    # spatial join stations to rail lines,
+    # remove stations outside search radius
+    sjoin = gpd.sjoin_nearest(
+        gdf_stations,
+        gdf_edges,
+        how="left",
+        rsuffix="",
+        distance_col="dist",
+        max_distance=cfg["geo"]["search_radius"],
+    ).sort_values("dist", ascending=True)
+    sjoin = sjoin[sjoin.dist.notna()]
+    sjoin = sjoin[~sjoin.index.duplicated(keep="first")]
+
+    # for every station find nearest rail line coord,
+    # union all points allocated to the same rail line
+    sjoin["n_pt"] = sjoin.apply(
+        lambda x: nearest_points(x.geometry, x.line_geom)[1], axis=1
+    )
+    sjoin["n_pts"] = sjoin.FID_rail_d.apply(
+        lambda x: sjoin[sjoin.FID_rail_d == x].n_pt.unary_union
+    )
+    # split lines into segments based on n_pts (MultiPoint)
+    sjoin["line_geom"] = sjoin.apply(
+        lambda x: split(snap(x.line_geom, x.n_pts, 100), x.n_pts), axis=1
+    )
+
+    # rebuild gdf_nodes
+    sjn_nodes = sjoin[gdf_stations.columns]
+    sjn_nodes["is_station"] = True
+    # station coords are shifted onto rail line
+    sjn_nodes["geometry"] = sjoin["n_pt"]
+    # when allocated duplicated, keep station coord and build connecting edges
+    idx_dpl = sjn_nodes[sjn_nodes.geometry.duplicated()].index
+    sjn_nodes.loc[idx_dpl, "geometry"] = gdf_stations.loc[idx_dpl, "geometry"]
+    con_edges = sjoin.loc[idx_dpl, gdf_edges.columns]
+    con_edges["geometry"] = [
+        LineString(geoms)
+        for geoms in zip(
+            gdf_stations.loc[idx_dpl, "geometry"], sjoin.loc[idx_dpl, "n_pt"]
+        )
+    ]
+    # handle indexing
+    idx_nde = [sjn_nodes.geometry.x, sjn_nodes.geometry.y]
+    sjn_nodes = sjn_nodes.set_index(idx_nde)
+
+    # rebuild gdf_edges
+    sjn_edges = sjoin[gdf_edges.columns]
+    sjn_edges["segmented"] = True
+    # overwrite gdf with segmented line geometries
+    sjn_edges["geometry"] = sjn_edges["line_geom"]
+    sjn_edges = sjn_edges.explode("geometry", index_parts=True)
+    sjn_edges = sjn_edges.drop_duplicates("geometry")
+    sjn_edges = pd.concat([sjn_edges, con_edges], axis=0)
+    sjn_edges["length"] = sjn_edges.geometry.length
+    # handle indexing
+    sjn_edges.rename(columns={"index_0": "u", "index_1": "v", "index_2": "k"})
+    sjn_edges["u"] = sjn_edges.apply(lambda x: x.geometry.coords[0], axis=1)
+    sjn_edges["v"] = sjn_edges.apply(lambda x: x.geometry.coords[-1], axis=1)
+    sjn_edges["key"] = 0
+    sjn_edges = sjn_edges.set_index(["u", "v", "key"])
+    sjn_edges = sjn_edges.drop("line_geom", axis=1)
+
+    # remove original lines from gdf
+    idx_drp = [tuple(idx) for idx in sjoin[["index_0", "index_1"]].values]
+    gdf_edges = gdf_edges.sort_index().drop(idx_drp, axis=0)
+
+    # merge old and new gdfs
+    gdf_nodes = pd.concat([gdf_nodes, sjn_nodes])
+    gdf_edges = pd.concat([gdf_edges, sjn_edges])
 
     return gdf_nodes, gdf_edges

3bd4a88d21eb23c6ffe2cb35e74f165e826fd0bf
Piaulous <paul.dubielzig@mailbox.org>
2023-12-18 18:28:50 +0100

    set geom precision in gdf

--- scripts/tools.py
+++ scripts/tools.py
@@ -1,6 +1,8 @@
 import logging
 
+import geopandas as gpd
 import yaml
+from shapely import wkt
 
 
 def read_config():
@@ -25,3 +27,14 @@ def setup_logger(name):
     logger.addHandler(handler)
 
     return logger
+
+
+def round_gdf(gdf):
+    precision = cfg["geo"]["precision"]
+
+    geo_cols = gdf.select_dtypes(include="geometry").columns
+    for col in geo_cols:
+        geoms = gdf.apply(lambda x: wkt.dumps(x[col], True, precision), axis=1)
+        gdf[col] = gpd.GeoSeries.from_wkt(geoms)
+
+    return gdf
